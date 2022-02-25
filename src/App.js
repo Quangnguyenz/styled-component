cb1a1718b44a0cbf876e474ae065f61fcddf3b91
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-02-24 21:52:33 -0600

    styled components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { DefaultButton, HipsterButton } from './components/Buttons'
 import { Button } from '@material-ui/core'
 import ComplexTitle from './components/ComplexTitle'
 import Random from './components/Random'
+import AlternativeTitle from './components/AlternativeTitle'
 
 const StyledBtn = styled(Button)`
 text-transform: capitalize;
@@ -13,6 +14,7 @@ function App() {
   return (
     <div style={{ padding: "2rem" }}>
       <ComplexTitle title='more complex title'></ComplexTitle>
+      <AlternativeTitle title='alternative title'></AlternativeTitle>
     </div>
   );
 }
--- src/components/ComplexTitle.js
+++ src/components/ComplexTitle.js
@@ -3,22 +3,27 @@ import styled from 'styled-components'
 
 const ComplexTitle = ({ title }) => {
     return (
-        <div>
+        <Wrapper className='bg-grey'>
             <h1>{title}</h1>
             <div className='underline'></div>
-        </div>
+        </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
 h1{
-text-transform: capitalize;
-text-align: center;
+    text-transform: capitalize;
+    text-align: center;
 }
 .underline{
-width: 5rem;
-height: 0.25rem;
-background: #645cff;
-margin: 0 auto;}
+    width: 5rem;
+    height: 0.25rem;
+    background: var(--primary);
+    margin: 0 auto;
+}
+.title{
+    color: blue
+}
 `
+
 export default ComplexTitle
