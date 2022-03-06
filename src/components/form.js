d7ed312f03a149fc4bd3f3069d7a9dc760f37011
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-06 17:53:39 -0600

    styled components

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import styled, { css } from 'styled-components/macro'
+
+const Button = styled.button.attrs((props) => {
+    return { type: props.type || 'button' }
+})`
+background:lightblue;
+border:none;
+color: white;
+padding: 0.25rem;
+cursor: pointer;
+${({ type }) => {
+        return (type === 'submit' && css`
+        display: block;
+        width:100%;
+        margin:1rem auto;
+        margin-top: 1rem;
+        border-radius: 0.25rem;
+        `)
+    }
+    }
+`
+
 function Form() {
     return (
         <div>
             <h2>some random stuff</h2>
-            <button>click me</button>
+
+            <Button type='button'>click me</Button>
             <form css={`width:300px;background:#fff;padding:2rem;margin-top:1rem`}>
                 <h2>Form</h2>
                 <input type="text" />
-                <button>submit here</button>
+                <Button type="submit">submit here</Button>
             </form>
         </div>
     )
