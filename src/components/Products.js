00009d76151cecd10b24783e7592a006db80d3a9
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2022-03-09 20:35:55 -0600

    styled components

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from './ComplexTitle'
+import Product from './Product'
 const products = [
     { id: 1, name: 'chair', price: 30 },
     { id: 2, name: 'bed', price: 150 },
@@ -10,6 +11,9 @@ const Products = () => {
     return (
         <section>
             <Title title="all products"></Title>
+            {products.map((product) => {
+                return <Product key={product.id} {...product} />
+            })}
         </section>
     )
 }
